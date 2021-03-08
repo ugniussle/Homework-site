20441f9ac08b702fc83fcfb968b335949572548a
ugniussle <79809483+ugniussle@users.noreply.github.com>
2021-03-08 10:17:43 +0200

    easier menu element addition

--- js/menu.js
+++ js/menu.js
@@ -6,3 +6,12 @@ function hide(){
 	document.getElementsByClassName('menu')[0].style.display='none';
 	console.log('away');
 }
+function createMenuEl(){
+	var links=["imageGame.html","tables.html","form.html","snake.html"];
+	var names=["Nuotraukos žaidimas","Spalvotas table","Anketa","Snake žaidimas"];
+	var insert="";
+	for(i=0;i<links.length;i++){
+		insert+="<a href=\""+links[i]+"\">"+names[i]+"</a><br>";
+	}
+	document.getElementById("menu").innerHTML=insert;
+}
