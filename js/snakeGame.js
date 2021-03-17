00063e634b312f2f8885461167b7c1be02e6cf8d
ugniussle <79809483+ugniussle@users.noreply.github.com>
2021-03-17 19:15:32 +0200

    add visualization

--- js/snakeGame.js
+++ js/snakeGame.js
@@ -1,8 +1,8 @@
-var dim;
+var dim; //dimensions of table
 function createTable(x){
     dim=x;
     if(dim<5||dim>50)return 1;
-    document.getElementById("tableDiv").innerHTML+="<table onmouseleave='gameEndOnMouseLeave();' id='table' style='border-collapse:collapse;cellpadding:0px'>";
+    document.getElementById("tableDiv").innerHTML+="<table onmouseleave='gameEndOnMouseLeave();' id='table' style='border-collapse:collapse;cellpadding:0px;background-color:black'>";
     for (i=0;i<dim;i++){
         document.getElementById("table").innerHTML+="<tr id=tr"+i+">";
         for (j=0;j<dim;j++){
@@ -21,33 +21,77 @@ function delTable(){
 }
 var snakeLength=0;
 var snakePos=[];
-function startGame(x,y){  //start the game
+function startGame(x,y){
     removeOnClick();
-    snakeLength=1;
+    snakeLength=3;
     snakePos[0]='x'+x+'y'+y;
     move(x,y);
     genFood();
 }
 function move(x,y){ //snake move function
     pos=snakePos.length-1;
-    if(snakeLength==0)return 1;
-    for(i=0;i<snakeLength;i++){ //check for collision
-        if(snakePos[i]==('x'+x+'y'+y)&&pos!=0){
+    if(snakeLength==0||'x'+x+'y'+y==snakePos[pos])return 1;
+    for(i=0;i<pos;i++){ //check for collision
+        if((snakePos[i]==('x'+x+'y'+y))&&(pos!=0)){
             gameEnd();
             document.getElementById("loseCond").innerHTML="Paskutinis žaidimas pralaimėtas dėl: suvalgei save";
             return 0;
-        };
+        }
     }
     if(pos+1<snakeLength){ //move snake when it is not full length
         snakePos[pos+1]='x'+x+'y'+y;
-        setGreenColor(snakePos[pos+1]);
+        setSnakeHead(snakePos[pos+1]);
+        if(pos+1==1)setSnakeTail(snakePos[pos]);
+        else if(pos+1>1)setSnakeBody(snakePos[pos]);
     }
     else if(pos+1==snakeLength){ //move snake when it is full length
         setBlackColor(snakePos[0]);
         for(i=0;i<pos;i++) snakePos[i]=snakePos[i+1];  // shift array to left
         snakePos[pos]='x'+x+'y'+y;
-        setGreenColor(snakePos[pos]);
+        setSnakeHead(snakePos[pos]);
+        if(pos>1){setSnakeBody(snakePos[pos-1]);}
+        if(snakeLength>1)setSnakeTail(snakePos[0]);
+        else setSnakeHead(snakePos[0]);
     }
+        var rotation=getRotation(pos);
+        rotate(rotation,snakePos[pos]);
+        document.getElementById(snakePos[0]).style.transform.value=document.getElementById(snakePos[1]).style.transform.value;
+}
+function getRotation(pos){
+    var x1=getYFromId(snakePos[pos]);
+    var y1=getXFromId(snakePos[pos]);
+    var x2=getYFromId(snakePos[pos-1]);
+    var y2=getXFromId(snakePos[pos-1]);  // x and y switched because x is rows and y is columns
+    var xDiff=x1-x2;
+    var yDiff=y2-y1;
+    console.log(xDiff+' '+yDiff);
+    switch(xDiff){
+        case -1:
+            switch(yDiff){
+                case -1:
+                    return -135;
+                case 0:
+                    return -90;
+                case 1:
+                    return 90;
+            }
+        case 0:
+            switch(yDiff){
+                case -1:
+                    return 180;
+                case 1: 
+                    return 0;
+            }
+            
+        case 1:
+            return 90;
+    }/*
+    switch(yDiff){
+        
+    }*/
+}
+function rotate(rot,id){
+    document.getElementById(id).style.transform="rotate("+rot+"deg)";
 }
 function gameEnd(){  //reset everything to a fresh state
     for(i=0;i<dim;i++){
@@ -61,10 +105,11 @@ function gameEnd(){  //reset everything to a fresh state
         document.getElementById("record").innerHTML="Paskutinio žaidimo taškai: "+(snakeLength-1);
     snakePos=[];
     snakeLength=0;
+    document.getElementById("score").innerHTML="Taškai: 0";
 }
 function gameEndOnMouseLeave(){  //set the lose condition(and end the game)
     if(snakeLength!=0) document.getElementById('loseCond').innerHTML='Paskutinis žaidimas pralaimėtas dėl: išėjei iš lango';
-    else return 1;
+    else return 0;
     gameEnd();
 }
 function gameEndWin(){
@@ -86,18 +131,18 @@ function genFood(){ //generate food square
         }
         if(count==snakePos.length)break;
     }
-    document.getElementById("x"+x+"y"+y).setAttribute("onmouseover",'eatFood('+x+','+y+');move('+x+','+y+')');  //set eatFood function on square
+    document.getElementById("x"+x+"y"+y).setAttribute("onmouseenter",';move('+x+','+y+');eatFood('+x+','+y+')');  //set eatFood function on square
     if(snakeLength%8!=0)
-        document.getElementById("x"+x+"y"+y).style="height:"+(window.innerHeight-450)/dim+"px;width:"+(window.innerHeight-450)/dim+"px;background-color:yellow;border:1px solid black";  //set yellow square
-    else document.getElementById("x"+x+"y"+y).style="height:"+(window.innerHeight-450)/dim+"px;width:"+(window.innerHeight-450)/dim+"px;background-color:purple;border:1px solid black";  //set purple square
+        document.getElementById("x"+x+"y"+y).style="height:"+(window.innerHeight-450)/dim+"px;width:"+(window.innerHeight-450)/dim+"px;background-color:yellow;border:0px solid black";  //set yellow square
+    else document.getElementById("x"+x+"y"+y).style="height:"+(window.innerHeight-450)/dim+"px;width:"+(window.innerHeight-450)/dim+"px;background-color:purple;border:0px solid black";  //set purple square
 }
 function eatFood(x,y){
     resetMouseOver(x,y); //unset eatFood function on square
     var purpleValue=document.getElementById("purpleValue").value;
     purpleValue=parseInt(purpleValue,10);
-    if(snakeLength%8!=0)
+    if(snakeLength%(5+Math.floor(purpleValue/2))!=0)
         snakeLength++;
-    else if (snakeLength%8==0&&purpleValue>0&&purpleValue<=10) snakeLength+=purpleValue;
+    else if (snakeLength%(5+Math.floor(purpleValue/2))==0&&purpleValue>0&&purpleValue<=10) snakeLength+=purpleValue;
     else {
         document.getElementById('loseCond').innerHTML='Paskutinis žaidimas pralaimėtas dėl: hax';
         gameEnd();
@@ -109,17 +154,54 @@ function eatFood(x,y){
     }
     genFood();
     document.getElementById("score").innerHTML="Taškai: "+(snakeLength-1);
-    document.getElementById("score").style.color='rgb('+(Math.random*256)+','+(Math.random*256)+','+(Math.random*256)+')';
+    //document.getElementById("score").style.color='rgb('+(Math.random*256)+','+(Math.random*256)+','+(Math.random*256)+')';
 }
 function removeOnClick(){
     for(i=0;i<dim;i++)
         for(j=0;j<dim;j++)
             document.getElementById("x"+i+"y"+j).setAttribute("onclick",'');
 }
-function createOnClick(x,y) { document.getElementById("x"+x+"y"+y).setAttribute("onclick",'startGame('+x+','+y+')'); }
-function resetMouseOver(x,y){ document.getElementById("x"+x+"y"+y).setAttribute("onmouseover",'move('+x+','+y+')'); }
-function setGreenColor(id){ document.getElementById(id).style="height:"+(window.innerHeight-450)/dim+"px;width:"+(window.innerHeight-450)/dim+"px;background-color:green;border:1px solid black"; }
-function setBlackColor(id){ document.getElementById(id).style="height:"+(window.innerHeight-450)/dim+"px;width:"+(window.innerHeight-450)/dim+"px;background-color:black;border:1px solid rgba(0,50,0,0.4);"; }
+function createOnClick(x,y) { document.getElementById("x"+x+"y"+y).setAttribute("onclick",'startGame('+x+','+y+')'); }  //add the start game function
+function resetMouseOver(x,y){ document.getElementById("x"+x+"y"+y).setAttribute("onmouseenter",'move('+x+','+y+')'); }   //reset move function on tile
+function setSnakeHead(id){ 
+    var sub=400;
+    document.getElementById(id).style.height=(window.innerHeight-sub)/dim;
+    document.getElementById(id).style.width=(window.innerHeight-sub)/dim;
+    document.getElementById(id).style.backgroundImage="url('img/snake/snakehead.png')";
+    document.getElementById(id).style.backgroundRepeat='no-repeat';
+    document.getElementById(id).style.backgroundSize=(window.innerHeight-sub)/dim+'px '+(window.innerHeight-sub)/dim+'px';
+    //document.getElementById(id).style="height:"+(window.innerHeight-sub)/dim+"px;width:"+(window.innerHeight-sub)/dim+"px;background-image:url('img/snake/snakehead.png')";
+    
+}
+function setSnakeBody(id){
+    var sub=400;
+    document.getElementById(id).style.height=(window.innerHeight-sub)/dim;
+    document.getElementById(id).style.width=(window.innerHeight-sub)/dim;
+    document.getElementById(id).style.backgroundImage="url('img/snake/snakebase.png')";
+    document.getElementById(id).style.backgroundRepeat='no-repeat';
+    document.getElementById(id).style.backgroundSize=(window.innerHeight-sub)/dim+'px '+(window.innerHeight-sub)/dim+'px';
+    //document.getElementById(id).style="height:"+(window.innerHeight-sub)/dim+"px;width:"+(window.innerHeight-sub)/dim+"px;background-size:fit;background-image:url('img/snake/snakebase.png')";
+    //document.getElementById(id).style.backgroundSize=(window.innerHeight-sub)/dim+'px '+(window.innerHeight-sub)/dim+'px';
+    //document.getElementById(id).style.backgroundRepeat='no-repeat';
+}
+function setSnakeTail(id){
+    var sub=400;
+    document.getElementById(id).style.height=(window.innerHeight-sub)/dim;
+    document.getElementById(id).style.width=(window.innerHeight-sub)/dim;
+    document.getElementById(id).style.backgroundImage="url('img/snake/snaketail.png')";
+    document.getElementById(id).style.backgroundRepeat='no-repeat';
+    document.getElementById(id).style.backgroundSize=(window.innerHeight-sub)/dim+'px '+(window.innerHeight-sub)/dim+'px';
+    //document.getElementById(id).style="height:"+(window.innerHeight-sub)/dim+"px;width:"+(window.innerHeight-sub)/dim+"px;background-size:fit;background-image:url('img/snake/snaketail.png')";
+    //document.getElementById(id).style.backgroundSize=(window.innerHeight-sub)/dim+'px '+(window.innerHeight-sub)/dim+'px';
+    //document.getElementById(id).style.backgroundRepeat='no-repeat';
+}
+function setBlackColor(id){
+    var sub=400;
+    document.getElementById(id).style="height:"+(window.innerHeight-sub)/dim+"px;width:"+(window.innerHeight-sub)/dim+"px;background-color:black;border:0px solid rgba(0,50,0,0.4);cellpadding:0px";
+}
+function getXFromId(id){return parseInt(id.substring(id.indexOf('x')+1,id.indexOf('y')),10); }
+function getYFromId(id){return parseInt(id.substring(id.indexOf('y')+1,id.length),10); } 
+
 
 //settings
 
