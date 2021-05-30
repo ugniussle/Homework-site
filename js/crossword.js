57ee9e46c5409bf007c4baf74d92bc5363a41c31
ugniussle <79809483+ugniussle@users.noreply.github.com>
2021-05-30 10:24:14 +0300

    add descriptions, other ui

--- js/crossword.js
+++ js/crossword.js
@@ -23,11 +23,12 @@ async function getWord(){
     ret[1]=data[0].definition
     return ret
 }*/
-var words=["word","width","disc","reset"];
-var pos=["x0y0","x0y0","x0y2","x2y0"];
-var direction=["right","down","right","down"];
-var descriptions=["a composition of letters"];
-var selectDirection;
+var words=["word","width","disc","reset","tangerine"];
+var pos=["x0y0","x0y0","x0y2","x2y0","x2y4"];
+var direction=["right","down","right","down","right"];
+var descriptions=["a composition of letters","how wide something is","Optical storage media","Restore something to initial state","Similar fruit to orange"];
+var selectDirection="";
+var X,Y;
 /*async function genWords(){
     for(i=0;i<10;i++){
         getWord()
@@ -66,7 +67,7 @@ function addWords(){
         for(j=0;j<words[i].length;j++){
             var id='x'+x+'y'+y;
             var el = document.getElementById(id)
-            el.innerHTML=`<input
+            if(el.innerHTML=="")el.innerHTML=`<input
              type='text' 
              maxlength='1' 
              size='1' 
@@ -74,10 +75,8 @@ function addWords(){
              id="in${id}"
              class="input"
              >`
-            //el.dataset.neededLetter=words[i][j]
             if(direction[i]=='right')x++
             else y++
-            
         }
     }
     addEvListeners()
@@ -85,25 +84,69 @@ function addWords(){
 function getX(id){return parseInt(id.substring(id.indexOf('x')+1,id.indexOf('y')),10); }
 function getY(id){return parseInt(id.substring(id.indexOf('y')+1,id.length),10); } 
 function addEvListeners(){
-    //var x=getX(pos[i]),y=getY(pos[i]);
     var els=document.getElementsByClassName("input")
-    for(i=0;i<els.length;i++){  //chack words
+    for(i=0;i<els.length;i++){
+        var el=document.getElementById(els[i].id)
+        el.tabIndex=1;
+    }
+    for(i=0;i<els.length;i++){  //all input elements loop
         var id=els[i].id;
         var el=document.getElementById(id)
         el.addEventListener("keyup",checkWords,false)
+        el.setAttribute("onfocus","X=getX(this.id);Y=getY(this.id)")
+        var cellId=id.substring(id.indexOf('x'),id.length)
+        for(j=0;j<words.length;j++)
+            if(cellId==pos[j])
+                el.tabIndex=0;
     }
-    for(i=0;i<words.length;i++){
-        var x=getX(pos[i]),y=getY(pos[i]);
-        for(j=0;j<words[i].length;j++){
-            var id='inx'+x+'y'+y;
-            var el = document.getElementById(id)
-            if(direction[i]=='right')x++
-            else y++
-            if(j+1<words[i].length){
-                el.setAttribute("onfocus","focusThat(this.id)")
-            }
+    genIndexes()
+}
+function genIndexes(){
+    let count=1;
+    for(i=0;i<pos.length;i++){
+        var el = document.getElementById(`dec${pos[i]}`)
+        if(typeof(el)=="undefined"||el==null){
+            var div=document.getElementById(pos[i])
+            div.innerHTML+=`<div id="dec${pos[i]}">${count}</div>`
+            div=document.getElementById(`dec${pos[i]}`)             //need to calculate vw
+            div.style.position="absolute"
+            div.style.marginTop="-7.3vh"
+            div.style.marginLeft="0.2vw"
+            div.style.fontSize="20px"
+            div.style.color="darkred"
+            count++
         }
     }
+    genDesc()
+}
+function genDesc(){
+    var htmlRight="Į dešinę:<br>";
+    var htmlDown="Į apačią:<br>";
+    for(i=0;i<words.length;i++){
+        var el=document.getElementById("dec"+pos[i])
+        var inEl=document.getElementById("in"+pos[i])
+        if(direction[i]=="right"&&isWordDisabled(i)==false){
+            htmlRight+=el.innerHTML+". "+descriptions[i]+"<br>"
+        }
+        else if(direction[i]=="down"&&isWordDisabled(i)==false){
+            htmlDown+=el.innerHTML+". "+descriptions[i]+"<br>"
+        }
+    }
+    document.getElementById("description").innerHTML="Žodžių apibūdinimai:<br>"+htmlRight+htmlDown
+}
+function isWordDisabled(i){
+    var x=getX(pos[i]),y=getY(pos[i]);
+    var count=0
+    for(j=0;j<words[i].length;j++){
+        var id='inx'+x+'y'+y;
+        var el = document.getElementById(id)
+        if(el.disabled==true)count++
+        if(direction[i]=='right')x++
+        else y++
+    }
+    if(count==words[i].length)return true
+    else return false
+    
 }
 function checkWords(){
     for(i=0;i<words.length;i++){
@@ -127,16 +170,10 @@ function checkWords(){
                 if(direction[i]=='right')x++
                 else y++
             }
-            selectDirection=null
+            
         }
-        /*else{
-            for(j=0;j<words[i].length;j++){
-                document.getElementById('inx'+x+'y'+y).style.border="2px solid black"
-                if(direction[i]=='right')x++
-                else y++
-            }
-        }*/
     }
+    genDesc()
 }
 function solveAll(){
     var els=document.getElementsByClassName("input")
@@ -156,34 +193,69 @@ function resetCross(){
     }
     checkWords()
 }
-function focusThat(id){
-    console.log(id)
-    //if(document.getElementById("in"+id)==null)return 1
-    //document.getElementById("in"+id).focus();
-    var s = id.substring(id.indexOf('x'),id.length)
-    var x=getX(s),y=getY(s);
-    for(i=0;i<words.length;i++){
-        if(s==pos[i]){
-            if(selectDirection==null&&direction[i]=="right"){
-                selectDirection="right"
-            }
-            else if(selectDirection==null&&direction[i]!="right"){
-                selectDirection="down"
-            }
-        }
+function focus(back){
+    var x=X,y=Y
+    if(back==0){
         if(selectDirection=="right"){
             x++
-            var selectedId="inx"+x+"y"+y;
-            document.getElementById(id).setAttribute("onkeyup","document.getElementById('"+selectedId+"').focus()")
-            console.log("focus to ",selectedId)
-            return
+            var id="inx"+x+"y"+y
+            while(idExists(x,y)&&document.getElementById(id).disabled==true)
+                id="inx"+(++x)+"y"+y
+            if(idExists(x,y)) document.getElementById(id).focus()
         }
         else if(selectDirection=="down"){
             y++
-            var selectedId="inx"+x+"y"+y;
-            document.getElementById(id).setAttribute("onkeyup","document.getElementById('"+selectedId+"').focus()")
-            console.log("focus to ",selectedId)
-            return
+            var id="inx"+x+"y"+y
+            while(idExists(x,y)&&document.getElementById(id).disabled==true)
+                id="inx"+x+"y"+(++y)
+            if(idExists(x,y))
+                document.getElementById(id).focus()
         }
+        else if(selectDirection=="") alert("Pasirinkite kryptį")
+    }
+    else if(back==1){
+        if(selectDirection=="right"){
+            x--
+            var id="inx"+x+"y"+y
+            while(idExists(x,y)&&document.getElementById(id).disabled==true)
+                id="inx"+(--x)+"y"+y
+            if(idExists(x,y)) document.getElementById(id).focus()
+            
+        }
+        else if(selectDirection=="down"){
+            y--
+            var id="inx"+x+"y"+y
+            while(idExists(x,y)&&document.getElementById(id).disabled==true)
+                id="inx"+x+"y"+(--y)
+            if(idExists(x,y)) document.getElementById(id).focus()
+        }
+        else if(selectDirection=="") alert("Pasirinkite kryptį")
     }
 }
+function idExists(x,y){
+    var el = document.getElementById(`inx${x}y${y}`)
+    if(typeof(el)=="undefined"||el==null)return false
+    else return true
+}
+document.addEventListener('keyup', function(event) {
+    if(event.code == "ArrowDown") {
+        selectDirection = "down"
+        document.getElementsByClassName("direction")[0].innerHTML=`<i class="fas fa-arrow-down"></i>`
+    }
+    else if(event.code == "ArrowRight") {
+        selectDirection = "right"
+        document.getElementsByClassName("direction")[0].innerHTML=`<i class="fas fa-arrow-right"></i>`
+    }
+    else if(event.key=="Backspace"){
+        focus(1)
+    }
+    else {
+        var alph="abcdefghijklmnopqrstuvwxyz"
+        for(i=0;i<alph.length;i++){
+            if(event.key==alph[i]){
+                focus(0)
+                return
+            }
+        }
+    }
+});
